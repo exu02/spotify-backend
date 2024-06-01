5dcd5b4f5751c1b6d05df5a996645975b03184a1
exu02 <xu.edward02@gmail.com>
2024-06-01 17:50:03 -0400

    adding cors handling and spotify authentication

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask, request
 from dotenv import find_dotenv, set_key, get_key
+from flask_cors import CORS, cross_origin
 import spotipy
 import spotipy.util as util
 from helpers import *
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content_Type"
 
 @app.route('/')
 def home():
     return 'API Server for Spotify Playlist Organizer'
 
 @app.route('/api/setUser', methods=['POST'])
+@cross_origin()
 def setUserAndAuth():
     userJson = request.get_json()
     user = userJson['username']
     set_key(find_dotenv(), 'USERNAME', user)
-    return auth
+    redirect_uri = "http://localhost:8080/"
+    client_id = get_key(find_dotenv(), "CLIENT_ID")
+    client_secret = get_key(find_dotenv(), "CLIENT_SECRET")
+    token = util.prompt_for_user_token(username=user, scope="playlist-read-private user-top-read user-library-read", client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    set_key(find_dotenv(), "CURR_USER_TOKEN", token)
+    return {"Message": "Success"}
 
 @app.route('/api/getPlaylists', methods=['GET'])
 def getPlaylists():
@@ -25,6 +34,7 @@ def getPlaylists():
     return playlists
 
 @app.route('/api/getTracksFromPlaylist', methods=['POST'])
+@cross_origin()
 def getTracksFromPlaylist():
     token = get_key(find_dotenv(), 'CURR_USER_TOKEN')
     sp = spotipy.Spotify(auth=token)
