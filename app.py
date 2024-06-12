ace3ba55bd74a9b7448a6e6c8b7005082725e92f
exu02 <xu.edward02@gmail.com>
2024-06-12 19:42:06 -0400

    added audio feature distance computations

--- app.py
+++ app.py
@@ -6,12 +6,22 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from helpers import *
 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content_Type'
 client_id = get_key(find_dotenv(), 'CLIENT_ID')
 client_secret = get_key(find_dotenv(), 'CLIENT_SECRET')
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+scaler = StandardScaler()
+feature_names = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+audio_feats_std = pd.DataFrame()
+track_df = pd.DataFrame()
 
 @app.route('/')
 def home():
@@ -23,29 +33,64 @@ def setUserAndAuth():
     userJson = request.get_json()
     user = userJson['username']
     set_key(find_dotenv(), 'USERNAME', user)
-    
-    # redirect_uri = 'http://localhost:8080/'
-    # token = util.prompt_for_user_token(username=user, scope='playlist-read-private user-top-read user-library-read', client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, show_dialog=True)
-    # set_key(find_dotenv(), 'CURR_USER_TOKEN', token)
 
     return {'spotify_username': user}
 
 @app.route('/api/getPlaylists', methods=['GET'])
 @cross_origin()
 def getPlaylists():
-    # token = get_key(find_dotenv(), 'CURR_USER_TOKEN')
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     playlists = sp.user_playlists(user=get_key(find_dotenv(), 'USERNAME'))['items']
     return playlists
 
 @app.route('/api/getTracksFromPlaylist', methods=['POST'])
 @cross_origin()
 def getTracksFromPlaylist():
-    # token = get_key(find_dotenv(), 'CURR_USER_TOKEN')
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    global track_df
+    global audio_feats_std
+
     playlist_id = request.get_json()['playlist_id']
     tracks = sp.playlist_items(playlist_id, fields='items(track(id, name, artists(name, id), album(name, id, images)))')['items']
-    return tracks
+    tracks = [tr['track'] for tr in tracks]
+    track_df = pd.DataFrame(tracks)
+    audio_feats = pd.DataFrame(sp.audio_features(track_df['id']))[feature_names]
+    audio_feats_std = pd.DataFrame(
+        scaler.fit_transform(audio_feats), 
+        columns=audio_feats.columns
+    )
+    audio_feats_std.index = track_df['id']
+    audio_feats_std.index.name = 'track_id'
+    track_df = track_df.merge(
+        right=audio_feats_std,
+        how='inner',
+        left_on='id',
+        right_index=True
+    )
+    track_df['distance'] = track_df.index.copy()
+    return track_df.to_dict('records')
+
+@app.route('/api/analyzePlaylist', methods=['POST'])
+@cross_origin()
+def analyzePlaylist():
+    global track_df
+
+    track_df = track_df.drop('distance', axis=1)
+    selected_tracks = request.get_json()
+    centroid = computeCentroid(
+        selected_tracks=selected_tracks,
+        audio_feats=audio_feats_std
+    )
+    dists = computeDistances(
+        audio_feats=audio_feats_std,
+        centroid=centroid
+    )
+    track_df = track_df.merge(
+        dists,
+        how='inner',
+        left_on='id',
+        right_on='track_id'
+    )
+
+    return track_df.to_dict('records')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,6 +1,9 @@
 from dotenv import find_dotenv, set_key, get_key
 import spotipy
 import spotipy.util as util
+import numpy as np
+import pandas as pd
+from sklearn.metrics import euclidean_distances
 
 def auth():
     client_id = get_key(find_dotenv(), 'CLIENT_ID')
@@ -11,3 +14,18 @@ def auth():
     token = util.prompt_for_user_token(username=username, scope='playlist-read-private user-top-read user-library-read', client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
     
     return set_key(find_dotenv(), 'CURR_USER_TOKEN', token)
+
+def computeCentroid(selected_tracks: list, audio_feats: pd.DataFrame):
+    track_ids = pd.Index([tr['track']['id'] for tr in selected_tracks])
+    selected_audio_feats = audio_feats.loc[track_ids]
+    centroid = selected_audio_feats.drop(['track_id'],axis=1).mean(axis=0)
+
+    return centroid
+
+def computeDistances(audio_feats: pd.DataFrame, centroid: pd.DataFrame):
+    dists = euclidean_distances(audio_feats, [centroid])
+    distDf = pd.DataFrame(dists, columns=["distance"])
+    distDf['track_id'] = audio_feats.index.copy()
+    
+    return distDf
+
